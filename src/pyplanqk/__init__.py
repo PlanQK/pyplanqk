a5be752750197893119589c560f18a1b3f89380a
Henning Krause <hgke@gft.com>
2023-08-18 14:17:38 +0200

    Add funcName to logger.

--- src/pyplanqk/__init__.py
+++ src/pyplanqk/__init__.py
@@ -6,7 +6,7 @@ from pyplanqk.high_level_actions import PyPlanQK
 name = "pyplanqk"
 level = logging.DEBUG
 
-default_frmt_str = "%(asctime)s | [%(levelname)s] | %(name)s | %(message)s"
+default_frmt_str = "%(asctime)s | [%(levelname)s] | %(name)s.%(funcName)s | %(message)s"
 formatter = logging.Formatter(default_frmt_str)
 
 stream_handler = logging.StreamHandler()
